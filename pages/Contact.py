4bc838c4b9afa5f4aa73ccf5a791dc8c50aed9b4
Sohan Katragadda <katragaddasohan@gmail.com>
2023-10-27 15:16:44 +0530

    Added a contact page with working email,number and address

---
+++ pages/Contact.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from contactemail import send_email
+
+st.header("Contact us")
+
+
+with st.form(key="Email_form"):
+    email = st.text_input("Enter email")
+    message = st.text_area("Enter your message")
+    button = st.form_submit_button("Submit")
+    print(email)
+    print(message)
+    if button:
+        message = f"""\
+Subject: User submitted contact
+
+From {email}:
+    {message}
+
+end of message
+"""
+        print(message)
+        send_email(message)
+
+st.title("Or Call us at 82312 48021")
+
+st.write("""Address: FJPG+FRQ, TPS Colony, Nagaram, Secunderabad, Telangana 501301""")
